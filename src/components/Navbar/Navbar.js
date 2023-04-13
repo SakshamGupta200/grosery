3e9bbdd67d26c2033669470ff70c51e5891b2fdd
Saksham Gupta <121724970+SakshamGupta200@users.noreply.github.com>
2023-04-13 19:56:56 +0530

    commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="s1">
+            
+        </div>
+        <div className="s1"></div>
+    </nav>
+  )
+}
+
+export default Navbar
