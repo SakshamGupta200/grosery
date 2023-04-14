a89c7fe136d7adb254e43ad0aecf6c0ec85e4ae3
Saksham Gupta <121724970+SakshamGupta200@users.noreply.github.com>
2023-04-14 16:58:13 +0530

    comit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Route, Routes, BrowserRouter} from 'react-router-dom';
 import Home from './PAGES/HomePage/Home';
-
+import './App.css';
 const App = () => {
   return (
     <div>
