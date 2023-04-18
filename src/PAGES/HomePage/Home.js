81258957ae97abb8ba6bdca116223d6c4324d807
Saksham Gupta <121724970+SakshamGupta200@users.noreply.github.com>
2023-04-18 08:58:38 +0530

    commit

--- src/PAGES/HomePage/Home.js
+++ src/PAGES/HomePage/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import BannerSlder from '../../components/Banner/BannerSlder'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <BannerSlder />
       Home
     </div>
   )
